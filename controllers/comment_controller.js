e7b516166c29508ff976496dd11dd44021db8e56
Pavononer17 <arturo.pavon.estrade@alumnos.upm.es>
2015-05-24 19:26:19 +0000

    Vueltoal22portonto

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,54 +1,77 @@
 var models = require('../models/models.js');
 
-// Autoload :id de comentarios
+// MW que permite acciones solamente si el quiz objeto
+// pertenece al usuario logeando o si es cuenta admin
+exports.ownershipRequired = function(req, res, next){
+  models.Quiz.find({
+    where : {
+      id: Number(req.commet.QuizId)
+    }
+  }).then(function(quiz){
+      if(quiz) {
+      var objQuizOwner = req.quiz.UserId;
+      var logUser = req.session.user.id;
+      var isAdmin = req.session.user.isAdmin;
+
+      console.log(objQuizOwner, logUser, isAdmin);
+
+  if(isAdmin || objQuizOwner === logUser) {
+    next();
+  } else {
+    res.redirect('/');
+  }
+ } else {next(new Error ('No existe quizId= ' + quizId))}
+}
+).catch(function(error){next(error)});
+};
+
+//Autoload :id de comentarios
 exports.load = function(req, res, next, commentId) {
   models.Comment.find({
-            where: {
-                id: Number(commentId)
-            }
-        }).then(function(comment) {
-      if (comment) {
-        req.comment = comment;
-        next();
-      } else{next(new Error('No existe commentId=' + commentId))}
+    where: {
+      id: Number(commentId)
     }
+  }).then(function(comment) {
+    if(comment) {
+      req.comment = comment;
+      next();
+    } else {next (new Error('No existe commentId' + commentId))}
+  }
   ).catch(function(error){next(error)});
 };
 
-// GET /quizes/:quizId/comments/new
+//GET /quizes/:quizId/comments/new
 exports.new = function(req, res) {
-  res.render('comments/new.ejs', {quizid: req.params.quizId, errors: []});
+  res.render ('comments/new.ejs', {quizid:  req.params.quizId, errors: []});
 };
 
 // POST /quizes/:quizId/comments
-exports.create = function(req, res) {
+exports.create=function(req, res) {
   var comment = models.Comment.build(
-      { texto: req.body.comment.texto,          
-        QuizId: req.params.quizId
-        });
+    { texto: req.body.comment.texto,
+      QuizId: req.params.quizId
+    });
 
   comment
   .validate()
   .then(
     function(err){
-      if (err) {
-        res.render('comments/new.ejs', {comment: comment, errors: err.errors});
-      } else {
-        comment // save: guarda en DB campo texto de comment
-        .save()
-        .then( function(){ res.redirect('/quizes/'+req.params.quizId)}) 
-      }      // res.redirect: Redirección HTTP a lista de preguntas
-    }
-  ).catch(function(error){next(error)});
-  
+    if(err) { 
+      res.render('comments/new.ejs',{comment:  comment, errors: err.errors});
+    } else {
+      comment // save: guarda en DB campo texto de comment
+      .save()
+      .then( function(){ res.redirect('/quizes/' + req.params.quizId)})
+    } // res.redirect:  Redirección HTTP a lista de preguntas
+     }
+    ).catch(function(error){next(error)}); 
 };
 
 // GET /quizes/:quizId/comments/:commentId/publish
 exports.publish = function(req, res) {
   req.comment.publicado = true;
 
-  req.comment.save( {fields: ["publicado"]})
-    .then( function(){ res.redirect('/quizes/'+req.params.quizId);} )
-    .catch(function(error){next(error)});
-
-  };
+  req.comment.save( { fields: ["publicado"]})
+  .then( function(){ res.redirect('/quizes/'+req.params.quizId);})
+  .catch(function(error){next(error)});
+};
