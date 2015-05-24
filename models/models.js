458100979ec90533d68e5386a5cfc4a87c92d64a
Pavononer17 <arturo.pavon.estrade@alumnos.upm.es>
2015-05-24 18:00:03 +0000

    Lamadredel20

--- models/models.js
+++ models/models.js
@@ -29,27 +29,46 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 // Importar definicion de la tabla Quiz
 var quiz_path = path.join(__dirname,'quiz');
 var Quiz = sequelize.import(quiz_path);
+var user_path = path.join(__dirname, 'user');
 
 // Importar definicion de la tabla Comment
 var comment_path = path.join(__dirname,'comment');
 var Comment = sequelize.import(comment_path);
+var User = sequelize.import(user_path);
 
 Comment.belongsTo(Quiz);
 Quiz.hasMany(Comment);
 
 exports.Quiz = Quiz; // exportar tabla Quiz
 exports.Comment = Comment;
+exports.User= User;
+
+Quiz.belongsTo(User);
+User.hasMany(Quiz);
 
 // sequelize.sync() inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
   // then(..) ejecuta el manejador una vez creada la tabla
-  Quiz.count().then(function (count){
-    if(count === 0) {   // la tabla se inicializa solo si está vacía
-      Quiz.bulkCreate( 
-        [ {pregunta: 'Capital de Italia',   respuesta: 'Roma'},
-          {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
-        ]
-      ).then(function(){console.log('Base de datos inicializada')});
-    };
+  User.count().then(function(count){
+    if (count === 0) { // la tabla se inicializa solo si está vacia
+      User.bulkCreate(
+        [ {username: 'admin', password:'1234', isAdmin: true},
+          {username: 'pepe', password: '5678'} // isAdmin por defecto: 'false'
+          ]
+        ).then(function(){
+          console.log('Base de datos (tabla user) inicializada');
+        Quiz.count().then(function(count){
+           if(count === 0) { // la tabla se inicializa solo si está vacia
+            Quiz.bulkCreate(
+              [ {pregunta: 'Capital de Italia',
+                    respuesta: 'Roma', UserId: 2},
+              {pregunta: 'Capital de Portugal',
+                    respuesta: 'Lisboa', UserId: 2}
+                  ]
+             ).then(function(){console.log('Base de datos inicializada')});
+           };
+         });
+       });
+      };
+    });
   });
-});
